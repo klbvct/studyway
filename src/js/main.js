6f4a8a7e51003824a836c5bbc971b7606d42e326
Viktor Kalabukhov <kalabukhov87@gmail.com>
2019-08-27 10:13:06 +0300

    create project

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+
+/*Кнопка Контакты в header*/
+var menuElem = document.getElementById('sweeties');
+var titleElem = menuElem.querySelector('.title');
+
+titleElem.onclick = function() {
+    menuElem.classList.toggle('open');
+};
+
