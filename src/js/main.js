585250135cf6d02c33a165d0d0fadcf26098d423
Viktor Kalabukhov <kalabukhov87@gmail.com>
2019-09-05 14:51:54 +0300

    create button in section Price

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,42 @@ titleElem.onclick = function() {
 };
 
 /*Колонки Price одной высоты*/
-var pi2 = $("div.price-item-2").height();
-$("div.price-item-1").height(pi2);
-$("div.price-item").height(pi2);
+$(document).ready(function(){
+    $.fn.equivalent = function (){
+            //запишем значение jQuery выборки к которой будет применена эта функция в локальную переменную $blocks
+        var $blocks = $(this),
+            //примем за максимальную высоту - высоту первого блока в выборке и запишем ее в переменную maxH
+            maxH    = $blocks.eq(2).height(); 
+
+        //делаем сравнение высоты каждого блока с максимальной
+        $blocks.each(function(){
+            maxH = ( $(this).height() > maxH ) ? $(this).height() : maxH;
+        });
+
+        //устанавливаем найденное максимальное значение высоты для каждого блока jQuery выборки
+        $blocks.height(maxH); 
+    }
+
+    //применяем нашу функцию в элементам jQuery выборки - $('.nav')
+    $('.price-item').equivalent(); 
+});
+/*Колонки Price ul одной высоты */
+$(document).ready(function(){
+    $.fn.equivalent = function (){
+            //запишем значение jQuery выборки к которой будет применена эта функция в локальную переменную $blocks
+        var $blocks = $(this),
+            //примем за максимальную высоту - высоту первого блока в выборке и запишем ее в переменную maxH
+            maxH    = $blocks.eq(2).height(); 
+
+        //делаем сравнение высоты каждого блока с максимальной
+        $blocks.each(function(){
+            maxH = ( $(this).height() > maxH ) ? $(this).height() : maxH;
+        });
+
+        //устанавливаем найденное максимальное значение высоты для каждого блока jQuery выборки
+        $blocks.height(maxH); 
+    }
+
+    //применяем нашу функцию в элементам jQuery выборки - $('.nav')
+    $('.price-item ul').equivalent(); 
+});
